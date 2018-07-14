751a71fe92f45f1b72ddce3ba28cbcd43e5f6f7a
omegafishy <zhiyulling@gmail.com>
2018-07-14 17:52:25 +0800

    first attempt at integrating leaflet map

--- realworkspace/src/components/Map/Map.js
+++ realworkspace/src/components/Map/Map.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import './Map.css';
 
-const Map = () => {
-    return (
-        <div id="map">
-            <iframe 
-                id="map-inner"
-                width="100%"
-                height="100%"
-                frameBorder="0" 
-                src="https://wesleynsc.carto.com/builder/7065d609-4e40-42c4-9839-329a94c5dcd4/embed" 
-                allowFullScreen 
-                webkitallowfullscreen="true" 
-                mozallowfullscreen="true"
-                oallowfullscreen="true"
-                msallowfullscreen="true">
-                </iframe>
-        </div>
-    )
-};
+class Map extends React.Component {
+
+}
+
+
+
 
 export default Map;
