b50ab6cb470b256a0ca1d5106196a0e9e632eee0
omegafishy <zhiyulling@gmail.com>
2018-07-14 18:42:58 +0800

    second attempt at integrating leaflet map

--- realworkspace/src/components/Map/Map.js
+++ realworkspace/src/components/Map/Map.js
@@ -1,7 +1,60 @@
 import React from 'react';
+import { Map, TileLayer} from 'react-leaflet';
 import './Map.css';
+import carto from '@carto/carto.js';
+import L from 'leaflet';
+
+
+var CARTO_BASEMAP = 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png';
 
 class Map extends React.Component {
+
+    state = {
+        center: [1.3521, 103.8198],
+        zoom: 13,
+        nativeMap: undefined,
+        layerStyle: MapStyle.style,
+        hidelayers: true
+    }    
+
+    client = new carto.Client({
+		apiKey: 'cefaa4464d8aba5e9a3afff2d22cea54e15990c2',
+		username: 'wesleynsc'
+    });
+
+    componentDidMount(){
+	// 	// set state of map
+    //     this.setState({ nativeMap: this.nativeMap });
+    //     // create layer base on current_BTO and to create pop up based on it
+	// 	L.geoJSON(current_BTO, {
+	// 	onEachFeature: function (f, l) {
+	// 	  l.bindPopup('<pre>'+JSON.stringify(f.properties,null,' ').replace(/["]/g,'')+'</pre>');
+	// 	}
+    //    }).addTo(this.nativeMap);	
+    
+
+
+
+
+	}
+
+
+	render(){
+		const { center, nativeMap, zoom } = this.state;
+		return (
+		<main>
+			<Map 
+				center = {center}
+				zoom = {zoom}
+				ref = {node => {this.nativeMap = node && node.leafletElement}}>
+			<TileLayer 
+				attribution = ""
+				url = {CARTO_BASEMAP} />
+			</Map>	
+		</main>
+		);
+	}
+    
 
 }
 
---
+++ realworkspace/src/desktop/index.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { Map, TileLayer as Basemap } from 'react-leaflet';
+import carto from 'carto.js';
+import Layer from './components/Layer';
+import Histogram from './components/Histogram';
+import airbnb from './data/airbnb';
+import utils from './utils/index';
+import './index.css';
+
+const CARTO_BASEMAP = 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}.png';
+
+class App extends Component {
+  state = {
+    center: [40.42, -3.7],
+    zoom: 13,
+    nativeMap: undefined,
+    layerStyle: airbnb.style,
+    hidelayers: true
+  }
+
+  cartoClient = new carto.Client({ apiKey: 'DEMO_API_KEY', username: 'ramirocartodb' });
+
+  componentDidMount() {
+    this.setState({ nativeMap: this.nativeMap });
+  }
+
+  renderHistogram = () => (
+    <Histogram
+      client={this.cartoClient}
+      source={airbnb.source}
+      nativeMap={this.state.nativeMap}
+      onDataChanged={this.onHistogramChanged.bind(this)}
+    />
+  )
+
+  // The widget returns an histogram, so we update the layer asigning a color to each histogram bin
+  onHistogramChanged(data) {
+    const newStyle = utils.buildStyle(data);
+    this.setState({ layerStyle: newStyle, hidelayers: false })
+  }
+
+  render() {
+    const { center, nativeMap, zoom } = this.state;
+
+    return (
+      <main>
+        <Map center={center} zoom={zoom} ref={node => { this.nativeMap = node && node.leafletElement }}>
+          <Basemap attribution="" url={CARTO_BASEMAP} />
+
+          <Layer
+            source={airbnb.source}
+            style={this.state.layerStyle}
+            client={this.cartoClient}
+            hidden={this.state.hidelayers}
+          />
+        </Map>
+
+        {nativeMap && this.renderHistogram()}
+      </main>
+    );
+  }
+
+
+}
+
+render(<App />, document.getElementById('root'));
+
+
+var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+  maxZoom: 17,
+  attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
+
+
+//http://leaflet-extras.github.io/leaflet-providers/preview/
